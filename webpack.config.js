d6e22df91c81a0ccbc03ea08cf9825aa43f215dc
Mike Peritz <mikeperitz@Mikes-MacBook-Air.local>
2021-07-05 20:47:11 -0400

    converted to spaces - whew

--- webpack.config.js
+++ webpack.config.js
@@ -4,55 +4,55 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
 
-    // entry files
-    entry: './src/client/app.ts',
+  // entry files
+  entry: './src/client/app.ts',
 
-    // output bundles (location)
-    output: {
-        path: path.resolve( __dirname, 'dist'),
-        filename: 'main.js',
-    },
+  // output bundles (location)
+  output: {
+    path: path.resolve( __dirname, 'dist'),
+    filename: 'main.js',
+  },
 
-    // file resolutions
-    resolve: {
-        extensions: [ '.ts', '.js' ],
-    },
+  // file resolutions
+  resolve: {
+    extensions: [ '.ts', '.js' ],
+  },
 
-    // loaders
-    module: {
-        rules: [
-            {
-                test: /\.ts/,
-                use: ['babel-loader', 'ts-loader'],
-                exclude: /node_modules/,
-                
-            },
-            {
-                test: /\.html$/,
-                use: [
-                  {
-                    loader: "html-loader",
-                  }
-                ]
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                  "style-loader",
-                  "css-loader",
-                  "sass-loader",
-                ],
-              },
+  // loaders
+  module: {
+    rules: [
+      {
+        test: /\.ts/,
+        use: ['babel-loader', 'ts-loader'],
+        exclude: /node_modules/,
+          
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          }
         ]
-    },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+    ]
+  },
 
-    plugins: [
-        new HtmlWebPackPlugin({
-          template: "./src/index.html",
-          filename: "./index.html"
-        })
-      ],
+  plugins: [
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html"
+      })
+    ],
 
-    // generate source map
-    devtool: 'source-map',
+  // generate source map
+  devtool: 'source-map',
 };
