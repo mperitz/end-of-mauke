2b850ffc5b5df09268c7f393ebf68cc3fbb28824
Mike Peritz <mikeperitz@Mikes-MacBook-Air.local>
2021-07-05 21:05:34 -0400

    sets up eslint and fixes existing lint errors

--- src/client/app.ts
+++ src/client/app.ts
@@ -1,8 +1,5 @@
-import "./style.scss";
+import './style.scss';
 
-console.log('hello');
 const app = document.getElementById('app');
-
-const myFunc = () : string => true ? 'yes' : 'no';
 
 app.innerHTML = '<h1>Hi!!</h1>';
