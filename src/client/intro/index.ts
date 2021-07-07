f641a45d0f15ac9283ccf37430230bf3b1b3af9e
Mike Peritz <mikeperitz@Mikes-MacBook-Air.local>
2021-07-06 21:07:53 -0400

    adds opacity transition and removal of start container on intro page start

--- src/client/intro/index.ts
+++ src/client/intro/index.ts
@@ -1,9 +1,24 @@
 import State from '../data_structures/state';
 
 export default function intro(state : State) : Promise<State> {
+  return new Promise(async (resolve : Function, reject : Function) => {
+    try {
+      await removeStartContainer();
+    } catch (err) {
+      reject(err);
+    }
+  });
+}
+
+function removeStartContainer() : Promise<void> {
   return new Promise((resolve : Function, reject : Function) => {
     try {
-
+      const startContainer = document.querySelector('.start-container');
+      startContainer.classList.add('fade-out');
+      setTimeout(() => {
+        startContainer.remove();
+        resolve();
+      }, 1000);
     } catch (err) {
       reject(err);
     }
